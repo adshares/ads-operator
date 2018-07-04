f9ab0f361e9d7e65267614d853bf4406a260eca6
Przemysław Furtak <przemyslawf@gmail.com>
2018-07-04 13:32:38 +0200

    1 setup project from sf4 (#2)
    #1 Initial commit with symfony and docker configuration

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController
+{
+    public function index()
+    {
+        return new Response(
+            '<html><body>Ads Manager</body></html>'
+        );
+    }
+}
