ef8acb109048fcdeb4ada04db864e35d3bf52ecc
Przemysław Furtak <przemyslawf@gmail.com>
2018-07-09 11:42:37 +0200

    #1 JMS Serializer and Mongo DB (Doctrine ODM) support (#6)
    * #1 JMS Serializer and Mongo DB (Doctrine ODM) support
    
    * #1 Support Mongo driver for Travis
    
    * #1 Support Mongo driver for Travis
    
    * #1 Support Mongo driver for Travis
    
    * #1 Support Mongo driver for Travis
    
    * #1 Support Mongo driver for Travis
    
    * #1 Support Mongo driver for Travis
    
    * #1 Support Mongo driver for Travis
    
    * #1 Support Mongo driver for Travis
    
    * #1 Cache PHP extensions
    
    * #1 Cache PHP extensions
    
    * #1 Cache PHP extensions
    
    * #1 Cache PHP extensions

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
