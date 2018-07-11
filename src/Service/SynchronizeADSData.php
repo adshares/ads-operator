a6e7875d120037ca2146c4ad8bf5cd58198ec5e0
Przemysław Furtak <przemyslaw.furtak@adshares.pl>
2018-07-11 08:13:32 +0200

    Use ADS client to fetch data from ads

--- src/Service/SynchronizeADSData.php
+++ src/Service/SynchronizeADSData.php
@@ -4,6 +4,10 @@
 namespace App\Service;
 
 use Adshares\Ads\AdsClient;
+use Adshares\Ads\Exception\CommandException;
+use Adshares\Ads\Response\GetBlockResponse;
+use Adshares\Ads\Response\GetBlocksResponse;
+use Adshares\Ads\Response\GetMessageListResponse;
 use Doctrine\MongoDB\Connection;
 
 class SynchronizeADSData
@@ -24,7 +28,30 @@ class SynchronizeADSData
         $this->database = $database;
     }
 
-    public function sync()
+    public function sync(): void
     {
+        try {
+            do {
+                $blocks = $this->client->getBlocks();
+                $this->parseBlocks($blocks->getBlocks());
+            } while ($blocks instanceof GetBlocksResponse);
+        } catch (CommandException $ex) {
+
+        }
+    }
+
+
+    private function parseBlocks(array $blocks)
+    {
+        foreach($blocks as $blockId) {
+            $block = $this->client->getBlock($blockId);
+            if ($block instanceof GetBlockResponse) {
+
+                $messageList = $this->client->getMessageList($blockId);
+                if ($messageList instanceof GetMessageListResponse) {
+
+                }
+            }
+        }
     }
 }
---
+++ tests/Service/SynchronizeADSDataTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Tests\Service;
+
+use PHPUnit\Framework\TestCase;
+
+final class SynchronizeADSDataTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress(): void
+    {
+        $this->assertTrue(true);
+    }
+}
