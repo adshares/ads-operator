bb65c4b12b59095045574c3c98f314499b3c93ca
Przemyslaw Furtak <przemyslawf@gmail.com>
2018-07-20 15:45:31 +0200

    Show block route and action

--- src/Controller/Blockexplorer/BlockController.php
+++ src/Controller/Blockexplorer/BlockController.php
@@ -4,10 +4,6 @@ namespace Adshares\AdsOperator\Controller\Blockexplorer;
 
 use Adshares\AdsOperator\Controller\ApiController;
 use Adshares\AdsOperator\Document\Block;
-use JMS\Serializer\DeserializationContext;
-use Swagger\Annotations as SWG;
-use Nelmio\ApiDocBundle\Annotation\Operation;
-use Nelmio\ApiDocBundle\Annotation\Model;
 
 class BlockController extends ApiController
 {
@@ -29,5 +25,30 @@ class BlockController extends ApiController
     public function listAction()
     {
         return $this->response($this->serializer->serialize([], 'json'));
+    }
+
+    /**
+     *
+     * @Operation(
+     *     summary="Get block details",
+     *     tags={"Block"},
+     *
+     *      @SWG\Response(
+     *          response=200,
+     *          description="Returned when operation is successful",
+     *          @Model(type=Block::class)
+     *      ),
+     *
+     *      @SWG\Parameter(
+     *          name="id",
+     *          in="path",
+     *          type="string",
+     *          description="Block identity"
+     *      )
+     * )
+     */
+    public function showAction()
+    {
+        return $this->response($this->serializer->serialize(new Block('1234'), 'json'));
     }
 }
