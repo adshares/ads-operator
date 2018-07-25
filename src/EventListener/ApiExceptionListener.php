c7ac518a9c6e2a57891d57d0e170dd96375f0fe9
Przemyslaw Furtak <przemyslawf@gmail.com>
2018-07-25 10:36:18 +0200

    Refactoring architecture, handle exceptions

---
+++ src/EventListener/ApiExceptionListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Adshares\AdsOperator\EventListener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ApiExceptionListener
+{
+    public function onKernelException(GetResponseForExceptionEvent $event): void
+    {
+        $exception = $event->getException();
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $data = [
+                'code' => $exception->getStatusCode(),
+                'message' => $exception->getMessage(),
+            ];
+
+            $response = new JsonResponse($data, $exception->getStatusCode());
+        } else {
+            $response = new JsonResponse(
+                [
+                    'code' => Response::HTTP_INTERNAL_SERVER_ERROR,
+                    'message' => 'Internal server error',
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+
+        $event->setResponse($response);
+    }
+}
