cbcfa5270b0889f7bda2648c2b6148ccbc2157d5
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-11-16 14:20:09 +0100

    Add php7.2-dev for mongo build

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -3,7 +3,7 @@
 set -e
 
 apt-get -qq -y --no-install-recommends install \
-    php7.2-fpm php-pear
+    php7.2-fpm php-pear php7.2-dev
 
 pecl install mongodb
 echo 'extension=mongodb.so' > /etc/php/7.2/mods-available/mongodb.ini
