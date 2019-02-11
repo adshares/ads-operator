46997def76f427183a8a70ac513d668692b7bcd5
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2019-02-11 16:42:40 +0100

    Show info field endpoint

--- src/Controller/Blockexplorer/InfoController.php
+++ src/Controller/Blockexplorer/InfoController.php
@@ -26,6 +26,7 @@ use Adshares\AdsOperator\Repository\InfoRepositoryInterface;
 use Swagger\Annotations as SWG;
 use Nelmio\ApiDocBundle\Annotation\Operation;
 use Nelmio\ApiDocBundle\Annotation\Model;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -76,5 +77,55 @@ class InfoController extends ApiController
         }
 
         return $this->response($this->serializer->serialize($info, 'json'), Response::HTTP_OK);
+    }
+
+    /**
+     * @Operation(
+     *     summary="Coin info field",
+     *     tags={"coin"},
+     *
+     *      @SWG\Response(
+     *          response=400,
+     *          description="Returned when query parameters are invalid"
+     *     ),
+     *     @SWG\Response(
+     *          response=200,
+     *          description="Returned when operation is successful",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref=@Model(type=Info::class))
+     *          )
+     *      ),
+     *     @SWG\Parameter(
+     *          name="field",
+     *          in="path",
+     *          type="string",
+     *          description="Field name"
+     *     )
+     * )
+     *
+     * @param Request $request
+     * @param string $field
+     * @return Response
+     */
+    public function showFieldAction(Request $request, string $field): Response
+    {
+        $info = $this->repository->getInfo($this->genesisTime);
+
+        if (!$info) {
+            throw new NotFoundHttpException(sprintf('The requested resource was not found'));
+        }
+
+        $getter = 'get' . ucfirst($field);
+        if (!method_exists($info, $getter)) {
+            throw new NotFoundHttpException(sprintf('The requested resource was not found'));
+        }
+
+        $data = $info->$getter();
+        if ($request->getContentType() === 'json') {
+            return $this->response($this->serializer->serialize($data, 'json'), Response::HTTP_OK);
+        }
+
+        return new Response($data, Response::HTTP_OK, ['content-type' => 'text/plain']);
     }
 }
