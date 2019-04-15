3f605b0ab1697d16c0c920fbf7901deab9a2ccea
Przemyslaw Furtak <przemyslawf@gmail.com>
2019-04-15 15:30:54 +0200

    OPR-29 Added missing files

---
+++ src/Command/CalculateInternalExchangeRateCommand.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Copyright (C) 2018 Adshares sp. z o.o.
+ *
+ * This file is part of ADS Operator
+ *
+ * ADS Operator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ADS Operator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ADS Operator.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+declare(strict_types=1);
+
+namespace Adshares\AdsOperator\Command;
+
+use Adshares\AdsOperator\Exchange\Currency;
+use Adshares\AdsOperator\Exchange\Exception\CalculationMethodRuntimeException;
+use Adshares\AdsOperator\UseCase\Exchange\CalculateInternalExchangeRate;
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CalculateInternalExchangeRateCommand extends ContainerAwareCommand
+{
+    private const CALCULATION_HOUR_PERIOD = 1;
+
+    /** @var CalculateInternalExchangeRate */
+    private $useCase;
+    /** @var Currency */
+    private $currency; // 1 hour
+
+    public function __construct(CalculateInternalExchangeRate $useCase, Currency $currency)
+    {
+        $this->useCase = $useCase;
+
+        parent::__construct();
+        $this->currency = $currency;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void
+    {
+        $date = new DateTime('-1 hour');
+        $date->setTime((int)$date->format('H'), 0);
+
+        $this
+            ->addOption(
+                'date',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Date for',
+                $date->format(DateTime::ATOM)
+            )
+            ->setName('ops:exchange:calculate')
+            ->setDescription('Importing exchange rate from provider');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        $startDate = DateTime::createFromFormat(DateTime::ATOM, $input->getOption('date'));
+        $endDate = (clone $startDate)->modify(sprintf('+%d hour', self::CALCULATION_HOUR_PERIOD));
+
+        $output->writeln(sprintf(
+            'Starting calculating hourly currency rate for %s to %s',
+            $startDate->format('Y-m-d H:i'),
+            $endDate->format('Y-m-d H:i')
+        ));
+
+        try {
+            $this->useCase->calculate($startDate, $endDate, $this->currency);
+        } catch (CalculationMethodRuntimeException $exception) {
+            $output->writeln(sprintf('[Error] %s', $exception->getMessage()));
+            exit();
+        }
+
+        $output->writeln(sprintf(
+            'Finished calculating an internal rate (%s) for %s.',
+            $this->useCase->getRateValue(),
+            $startDate->format('Y-m-d H:i')
+        ));
+    }
+}
---
+++ src/Exchange/Calculation/CalculationMedianMethod.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright (C) 2018 Adshares sp. z o.o.
+ *
+ * This file is part of ADS Operator
+ *
+ * ADS Operator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ADS Operator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ADS Operator.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+declare(strict_types=1);
+
+
+namespace Adshares\AdsOperator\Exchange\Calculation;
+
+use Adshares\AdsOperator\Exchange\Dto\ExchangeRate;
+use Adshares\AdsOperator\Exchange\Dto\ExchangeRateCollection;
+use Adshares\AdsOperator\Exchange\Exception\CalculationMethodRuntimeException;
+
+class CalculationMedianMethod implements CalculationMethodInterface
+{
+
+    public function calculate(ExchangeRateCollection $collection): float
+    {
+        $data = [];
+
+        /** @var ExchangeRate $item */
+        foreach ($collection as $item) {
+            $data[] = $item->getRate();
+        }
+
+        $count = count($data);
+
+        if ($count === 0) {
+            throw new CalculationMethodRuntimeException('No data to processed.');
+        }
+
+        $index = floor($count - 1 / 2);
+
+        return $data[$index];
+    }
+}
