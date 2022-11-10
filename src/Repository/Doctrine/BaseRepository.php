7a209307682b3dff4a60423bea5784be1027c813
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2022-11-10 16:02:47 +0100

    Add count to API

--- src/Repository/Doctrine/BaseRepository.php
+++ src/Repository/Doctrine/BaseRepository.php
@@ -1,21 +1,21 @@
 <?php
 /**
- * Copyright (C) 2018 Adshares sp. z o.o.
+ * Copyright (c) 2018-2022 Adshares sp. z o.o.
  *
  * This file is part of ADS Operator
  *
- * ADS Operator is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * ADS Operator is free software: you can redistribute and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * ADS Operator is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with ADS Operator.  If not, see <https://www.gnu.org/licenses/>
+ * along with ADS Operator. If not, see <https://www.gnu.org/licenses/>
  */
 
 namespace Adshares\AdsOperator\Repository\Doctrine;
@@ -40,6 +40,7 @@ class BaseRepository extends DocumentRepository
     public function fetchList(string $sort, string $order, int $limit, int $offset, array $conditions = []): array
     {
         $results = [];
+        $count = 0;
 
         try {
             $cursor = $this
@@ -54,18 +55,21 @@ class BaseRepository extends DocumentRepository
                 }
             }
 
-            $data = $cursor
+            $cursor = $cursor
                 ->getQuery()
-                ->execute()
-                ->toArray();
+                ->execute();
+            $data = $cursor->toArray();
+            $count = $cursor->count();
 
-            foreach ($data as $node) {
-                $results[] = $node;
+            foreach ($data as $item) {
+                $results[] = $item;
             }
         } catch (MongoDBException $ex) {
-            return [];
         }
 
-        return $results;
+        return [
+            'data' => $results,
+            'meta' => ['count' => $count]
+        ];
     }
 }
